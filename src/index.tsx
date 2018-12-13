1fe5c66338d0d7cec918cb931228f7f27194aed2
John Wiseheart <jwiseheart@palantir.com>
2018-12-13 17:49:42 +0000

    Complete example

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 // this needs to come first
 import 'reflect-metadata';
 // so lets stick it up here
-
 import { FrontendApplication } from '@theia/core/lib/browser';
 import { Container, ContainerModule } from 'inversify';
 import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
@@ -10,7 +9,6 @@ import { messagingFrontendModule } from '@theia/core/lib/browser/messaging/messa
 import { loggerFrontendModule } from '@theia/core/lib/browser/logger-frontend-module';
 import browserMenuModule from '@theia/core/lib/browser/menu/browser-menu-module';
 import { ThemeService } from '@theia/core/lib/browser/theming';
-
 
 class MyFrontendApplication extends FrontendApplication {
   protected getHost(): Promise<HTMLElement> {
@@ -24,7 +22,6 @@ class MyFrontendApplication extends FrontendApplication {
 
 const runApplication = () => {
   const frontendAppModule = new ContainerModule((bind, unbind, isBound, rebind) => {
-    // unbind(FrontendApplication);
     rebind(FrontendApplication).to(MyFrontendApplication).inSingletonScope();
   });
 
@@ -34,7 +31,7 @@ const runApplication = () => {
   })
 
   const container = new Container();
-  container.load(frontendAppModule)
+  container.load(frontendAppModule); // Commenting this line and the one below makes it work
   container.load(browserMenuModule);
   container.load(frontendApplicationModule);
   container.load(messagingFrontendModule);
@@ -45,7 +42,7 @@ const runApplication = () => {
       themeService.loadUserTheme();
 
       // const application = container.get(FrontendApplication);
-      const application = container.get(MyFrontendApplication);
+      const application = container.get(MyFrontendApplication);  // Commenting this line and the one above makes it work
       application.start();
   } catch (error) {
     console.error('Failed to start the frontend application.');
